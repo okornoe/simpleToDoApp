d49d42dd6f011dd704211c26dccd757a963b54cc
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2021-10-25 07:51:04 +0000

    Add task item adapter

---
+++ app/src/main/java/com/t/simpletodoapp/TaskItemAdapter.kt
@@ -0,0 +1,44 @@
+package com.t.simpletodoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Tells the recyclerview how to display the data pass to it
+ */
+class TaskItemAdapter(val listOfItems: List<String>) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        //Inflate the custom layout
+        val listItemView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(listItemView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listOfItems[position]
+        holder.textView.text = item
+    }
+
+    // Returns the total count of items in the list
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // store references to element in the layout view
+        val textView: TextView
+
+        init {
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+
+}

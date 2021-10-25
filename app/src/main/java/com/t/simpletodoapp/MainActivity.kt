9c7054c8b101a9cc977e4131fa93dc031704c8fb
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2021-10-25 08:06:31 +0000

    add items to the list using the add button

--- app/src/main/java/com/t/simpletodoapp/MainActivity.kt
+++ app/src/main/java/com/t/simpletodoapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.t.simpletodoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
 
         listOfTasks.add("Do assignment")
         listOfTasks.add("Work on the report")
@@ -27,5 +31,15 @@ class MainActivity : AppCompatActivity() {
         // Attach the adapter to the recyclerview to populate items
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // set the button and input field, do that the user can enter a task and add it to the list.
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val userInputTask =  inputTextField.text.toString()
+
+            listOfTasks.add(userInputTask)
+            adapter.notifyItemInserted(listOfTasks.size - 1)
+
+            inputTextField.setText("")
+        }
     }
 }

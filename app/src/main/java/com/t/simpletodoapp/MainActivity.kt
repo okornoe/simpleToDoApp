267ee605913a8f66daeeec4beb76622352141894
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2021-10-25 07:52:04 +0000

    render list of item to recycler view

--- app/src/main/java/com/t/simpletodoapp/MainActivity.kt
+++ app/src/main/java/com/t/simpletodoapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.t.simpletodoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listOfTasks.add("Do assignment")
+        listOfTasks.add("Work on the report")
+        listOfTasks.add("Sleep")
+
+        // Look up recyclerView in layout
+        val recyclerView =  findViewById<RecyclerView>(R.id.recyclerView)
+
+        // create adapter passing in the sample user data
+        val adapter = TaskItemAdapter(listOfTasks)
+
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
